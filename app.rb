71a8da405b7f8960b6711bdbd81d8792cc536c6e
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-03 05:05:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "holiday"
+  end
+
   get '/' do
     erb :index
   end
 
-  post '/checkout' do 
+  post '/checkout' do
+
+    params = {
+      :item => "socks"
+    }
     item = Item.new(params)
     session[:item_id] = item.id
   end
